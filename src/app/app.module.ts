4daff1a75ae2b306a82821f32006637b39487f91
Acceleratorrrr <542383480@qq.com>
2017-11-13 17:49:00 +0000

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
